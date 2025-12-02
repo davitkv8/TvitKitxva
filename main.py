be879f8b3416920f36d6450e6fccb606c878e0b9
Davit Kvaratskhelia <davit.kv8@gmail.com>
2025-12-03 02:56:50 +0400

    running host fix

--- main.py
+++ main.py
@@ -90,4 +90,9 @@ async def upload_pdf(file: UploadFile = File(...)):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True,  # for dev only
+    )
